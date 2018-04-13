14527708bdb45bba0785e99a2d8d9e87642f3ddb
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2018-04-13 02:10:35 -0400

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,10 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-content">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <h1 className="App-title">Pete App Designs</h1>
+        </div>
       </div>
     );
   }
